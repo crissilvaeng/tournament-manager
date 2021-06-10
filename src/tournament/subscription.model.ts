a81f8a902aa3e0bc173ae111f7af6abda34ea4a4
Rodrigo Filomeno <rodrigo.filomeno@radixeng.com.br>
2021-06-09 22:13:01 -0300

    Add 3 models

---
+++ src/tournament/subscription.model.ts
@@ -0,0 +1,23 @@
+import { Table, Model, Column, DataType } from "sequelize-typescript";
+
+@Table
+export class Subscription extends Model<Subscription> {
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false,
+    })
+    userId: string;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false,
+    })
+    agentId: string;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false,
+    })
+    tournamentId: string;
+}
---
+++ src/tournament/tournament.model.ts
@@ -0,0 +1,17 @@
+import { Table, Model, Column, DataType } from "sequelize-typescript";
+
+@Table
+export class Tournament extends Model<Tournament> {
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false,
+    })
+    name: string;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: true,
+    })
+    description: string;
+}
