e37750933f5646e6c288f503b0e9f024c8cb8710
Cristina Silva <crissilva.eng@gmail.com>
2021-06-11 08:34:26 +0000

    feat: put all endpoints under /api resource

--- src/main.ts
+++ src/main.ts
@@ -21,9 +21,11 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('docs', app, document);
 
   app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('api');
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
