847f9f5c42968768b8b8a9982632a88b0fa905bc
Rodrigo Filomeno <rodrigo.filomeno@radixeng.com.br>
2021-06-21 22:11:22 -0300

    Fix: Ajeitado maneira de passar mensagem pelo NATS
    Passando agora o objeto game para conseguir pegar o objeto já montado no microsserviço game-manager

--- src/tournaments/entities/game.entity.ts
+++ src/tournaments/entities/game.entity.ts
@@ -1,6 +1,16 @@
+import { IsNotEmpty, IsUUID } from 'class-validator';
+
 export class Game {
+  @IsUUID()
+  @IsNotEmpty()
   white: string;
+
+  @IsUUID()
+  @IsNotEmpty()
   black: string;
+
+  @IsUUID()
+  @IsNotEmpty()
   tournamentId: string;
 
   constructor(white: string, black: string, tornamentId: string) {
